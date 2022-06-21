db007ea067b25ef230ae69efd90b71f5da3c3783
Ale Gastaldi <adgastaldi@gmail.com>
2022-06-21 02:20:15 -0300

    Primera_entrega_Proyecto Final

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
 //Creo la clase de objeto producto como base para cargar los datos del participante     
 class participante {
-    constructor(nombre, apellido) {
+    constructor(nombre, apellido, puntaje) {
         this.nombre = nombre;
         this.apellido = apellido;
+        this.puntaje = puntaje;
         }
     }
 
     //Inicializo un array para cargar los participantes
 const listado = [];
 
-let entrada = prompt("Ingresar nombre jugador");
-let entrada2 = prompt("Ingresar apellido jugador");
-listado.push(new participante(entrada, entrada2));
+let playername = prompt("Ingresar nombre jugador N°1");
+let playerlastname = prompt("Ingresar apellido jugador N°1");
+let player2name = prompt("Ingresar nombre jugador N°2");
+let player2lastname = prompt("Ingresar apellido jugador N°2");
+let punt =0;
+let pun = 0;
+listado.push(new participante(playername, playerlastname, pun));
+listado.push(new participante(player2name, player2lastname, punt));
 
-for (const participante of listado) {
-    alert("PRIMER TURNO JUGADOR: " + participante.nombre + "  " + participante.apellido);
+for (const jugador of listado) {
+    alert("JUGADORES: " + jugador.nombre + "  " + jugador.apellido);
 }
 
-
+for (let i = 1; i < 3; i++){
 function opcion1(preg1){
+    
     if (respuesta == "1"){
         alert("FELICITACIONES, SU RESPUESTA ES CORRECTA!");
+        
+        return 1;
     }
     else if (respuesta !== "1") {
         for (let i = 0; i < 2; i++){
@@ -28,6 +37,8 @@ function opcion1(preg1){
             let respuesta = prompt(preg1);
             if (respuesta == "1"){
                 alert("FELICITACIONES, SU RESPUESTA ES CORRECTA!");
+                
+                return 1;
                 break;
             }
             else if (respuesta !== "1"){
@@ -39,11 +50,15 @@ function opcion1(preg1){
     }
     
     }
+    
     }
 
     function opcion2(preg2){
+        
         if (respuesta2 == "2"){
             alert("FELICITACIONES, SU RESPUESTA ES CORRECTA!");
+            
+            return 1;
         }
         else if (respuesta2 !== "2") {
             for (let i = 0; i < 2; i++){
@@ -51,6 +66,8 @@ function opcion1(preg1){
                 let respuesta2 = prompt(preg2);
                 if (respuesta2 == "2"){
                     alert("FELICITACIONES, SU RESPUESTA ES CORRECTA!");
+                    
+                    return 1;
                     break;
                 }
                 else if (respuesta2 !== "2"){
@@ -64,13 +81,37 @@ function opcion1(preg1){
         }
         }
 
+        
+let puntos = 0
 
+    alert(`TURNO JUGADOR N° ${i}`);
 let pregunta1 = "Vehículo de transporte aéreo provisto de alas. Escriba el numero de la opcion correcta: \n1-AVION\n2-TRACTOR\n3-DRONE\n4-HELICOPTERO" 
 let respuesta = prompt(pregunta1);
-opcion1(pregunta1);
+puntos = opcion1(pregunta1);
+
 
 let pregunta2 = "Local donde los hombres se cortaban y arreglaban el pelo, la barba y el bigote: \n1-CERVECERIA\n2-BARBERIA\n3-FERRETERIA\n4-RELOJERIA"
 let respuesta2 = prompt(pregunta2);
-opcion2(pregunta2);
+puntos = puntos + opcion2(pregunta2);
+alert ("SU PUNTAJE FUE DE: "+puntos);
+if (i === "1"){
+    pun = puntos;
+}
+else if (i === "2"){
+    punt = puntos;
+}
+
+}
+for (const jugador of listado) {
+    alert("JUGADORES: " + jugador.nombre + "  " + jugador.apellido+ "  PUNTAJE: " +jugador.puntaje);
+}
+
+const total = listado. reduce((acc, el) => acc + el. apellido, 0)
+console. log(total) 
+
+
+const players = listado. map((player) => player.nombre)
+console. log(players)
+
 
 
